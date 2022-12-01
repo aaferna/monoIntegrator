372f160975108bd2148312a401978c5d1d4a9e27
Agustin Fernandez <aaferna@outlook.com.ar>
2022-12-01 19:33:07 -0300

    1.1.2
    Se mejora el mensaje de fail Req

--- core/modules/express.js
+++ core/modules/express.js
@@ -1,5 +1,7 @@
 exports.jsonErrorHandler = async (err, req, res, next) => {
-    log4j.log("warn", `IP Origen: ${req.headers['x-forwarded-for'] || req.socket.remoteAddress }, Endpoint: ${req.originalUrl} | Se enviaron datos que no estan formateados en JSON`)
-    res.json({ msg : "Se enviaron datos que no estan formateados en JSON" });
+    const { randomUUID } = require('crypto'),
+    idreq = randomUUID()
+    log4j.log("warn", `IP ${req.headers['x-forwarded-for'] || req.socket.remoteAddress } :: Method ${ req.method } :: Endpoint ${req.originalUrl} :: IDREQ ${idreq} :: Se enviaron datos que no estan formateados en JSON`)
+    res.status(400).json({ msg: "Existe un inconveniente en la solicitud", idreq: idreq}) 
 }
 
