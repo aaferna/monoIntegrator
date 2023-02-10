3eb17e9a0ef0549d9cc49d8dc57ce25d75f53fc1
Agustin Fernandez <aaferna@outlook.com.ar>
2023-02-10 12:17:14 -0300

    2.0.0

--- server.js
+++ server.js
@@ -2,16 +2,22 @@ const express = require("express");
 const cors = require('cors')
 const app = express();
 const helmet = require('helmet')
-const { jsonErrorHandler, notFoundHandler, DDOSBlock } = require("./modules/expressHandle");
+const { jsonErrorHandler, notFoundHandler, DDOSBlock, reqInfo } = require("./modules/expressHandle");
+const { endpointMiddleware } = require("./modules/analitycs/mdw");
 
 global.executeSQL = require("./modules/sql").executeSQL
 global.executeSQLS = require("./modules/sql").executeSQLS
+global.reqInfo = reqInfo
 global.router = express.Router()
 
 app.use(express.json())
-app.use(jsonErrorHandler)
 app.use(helmet())
 app.use(cors({ origin: '*' }));
+app.use(jsonErrorHandler)
+
+app.use(endpointMiddleware);
+
+
 app.use(function(req, res, next) {
     res.setHeader('x-process-response', process.pid)
     next();
@@ -33,23 +39,23 @@ try {
         return filelist;
     };
   
-    const files = walkSync(dirRout);
-        files.forEach(file => {
-            if (file.endsWith(".js")) {
-                const route = require(`./${file}`);
-                app.use(route);
-            }
-        });
+    const   files = walkSync(dirRout);
+            files.forEach(file => {
+                if (file.endsWith(".js")) {
+                    const route = require(`./${file}`);
+                    app.use(route);
+                }
+            });
   
 } catch (error) {
     log("error", `Existe un inconveniente al importar un paquete de rutas :: ${error}`);
 }
     
 app.get('/status', (req, res) => {
-    
     res.json({ "keep" : "alive" }) 
 })
 
+app.use(require("./modules/analitycs/route"))
 app.use(notFoundHandler)
 
 module.exports = app
